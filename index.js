8d10c056ec6f2c76277b8c5d4c185eacb7b2c8bd
ekashatakishvili <157217955+ekashatakishvili@users.noreply.github.com>
2024-03-29 01:01:02 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+for (varindex=5; index>0; index--)
+{
+    console.log (index);
+}
+//
+var myAsigment
+if(myAsigment=1){console.log ("kenti ricxvebi")}
+for (var index=0; index < 10; index++ )
+{
+    if ( index %2 !=0);}
+{
+    console.log (index);
+}
